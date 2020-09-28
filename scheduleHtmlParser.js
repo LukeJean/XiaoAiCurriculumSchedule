34d921866f7b21da449acf7cf2cdb5d5b8b9daed
LukeJean <2601454425@qq.com>
2020-09-28 11:13:36 +0800

    '修改一个块块里两门课程相同但周数不同的bug'

--- scheduleHtmlParser.js
+++ scheduleHtmlParser.js
@@ -1,20 +1,20 @@
 function getTime(time) {
   let flag = 0;
-  if (typeof (time) == "undefined") {
-    return ["undefined", "undefined"];
+  if(typeof(time) == "undefined"){
+      return ["undefined", "undefined"];
   }
   let t = time.split('周)[');
   let week = []
   let weekStr = t[0];
   if (weekStr.search('单') != -1) {
-    flag = 1;
-    if (weekStr != null)
-      weekStr = weekStr.replace('单', '');
+      flag = 1;
+      if(weekStr != null)
+        weekStr = weekStr.replace('单', '');
   }
   if (weekStr.search('双') != -1) {
-    flag = 2;
-    if (weekStr != null)
-      weekStr = weekStr.replace('双', '');
+      flag = 2;
+      if(weekStr != null)
+        weekStr = weekStr.replace('双', '');
   }
   if (flag == 1) {
     week = weekStr2IntList(weekStr).filter(v => v % 2 != 0);
@@ -27,13 +27,13 @@ function getTime(time) {
   let start = 0;
   let end = 0;
   try {
-    if (t[1] != null) {
-      jc = t[1].replace('节]', '').split('-');
-      start = parseInt(jc[0]);
-      end = parseInt(jc[jc.length - 1]);
-    }
+      if(t[1] != null){
+          jc = t[1].replace('节]', '').split('-');
+          start = parseInt(jc[0]);
+          end = parseInt(jc[jc.length - 1]);
+      }
   } catch (e) {
-    console.log(e);
+      console.log(e);
   }
   let sections = []
 
@@ -52,37 +52,37 @@ function weekStr2IntList(week) {
 
   // 以逗号为界分割字符串，遍历分割的字符串
   week.split(",").forEach(w => {
-    if (w.search('-') != -1) {
-      let range = w.split("-");
-      let start = parseInt(range[0]);
-      let end = parseInt(range[1]);
-      for (let i = start; i <= end; i++) {
-        if (!weeks.includes(i)) {
-          weeks.push(i);
-        }
+      if (w.search('-') != -1) {
+          let range = w.split("-");
+          let start = parseInt(range[0]);
+          let end = parseInt(range[1]);
+          for (let i = start; i <= end; i++) {
+              if (!weeks.includes(i)) {
+                  weeks.push(i);
+              }
+          }
+      } else if (w.length != 0) {
+          let v = parseInt(w);
+          if (!weeks.includes(v)) {
+              weeks.push(v);
+          }
       }
-    } else if (w.length != 0) {
-      let v = parseInt(w);
-      if (!weeks.includes(v)) {
-        weeks.push(v);
-      }
-    }
   });
   return weeks;
 }
 
-function getCourse(info, col) {
+function getCourse(info,col){
   let course = {}
   course['name'] = info[0];
-  if (info.length == 4) {
+  if(info.length == 4){
     course['position'] = info[3]
-  } else {
+  }else{
     course['position'] = ''
   }
   course['teacher'] = info[1]
   let [weeks, sections] = getTime(info[2])
   course['weeks'] = weeks
-  course['day'] = col + 1
+  course['day'] = col+1
   course['sections'] = sections
   return course;
 }
@@ -96,101 +96,112 @@ function scheduleHtmlParser(html) {
     $('#kbtable').find('tr').each(function (row, _) {
       if (row != 0) {
         $(this).find('td').each(function (col, _) {
-          let info = $(this).html().match(reg)
-          if (info != null) {
-            info = info.slice(2, info.length - 1)
-            if (info.length >= 5) {
+            let info = $(this).html().match(reg)
+            if (info != null) {
+              info = info.slice(2, info.length - 1)
+              if (info.length >= 5) {
+                  
+                info = info.map((str) => str.trim())
+                let index = 0
+                var i = 0;
+                var count = 1;
 
-              info = info.map((str) => str.trim())
-              let index = 0
-              var i = 0;
-              var count = 1;
-
-              for (i = 0; i < info.length; i++) {
-                if (info[i] == '----------------------') {
-                  count = count + 1;
-                } else if (info[i] == "O") {
-                  info[i] = ""
-                } else if (info[i] == "P") {
-                  info[i] = ""
+                for(i = 0;i<info.length;i++){
+                   if(info[i] == '----------------------'){
+                      count = count + 1;
+                   }else if(info[i] == "O"){
+                      info[i]=""
+                   }else if(info[i] == "P"){
+                      info[i]=""
+                   }
                 }
-              }
+                    
+                //去掉info中的空值
+                info = info.filter(function (s) {
+                   return s && s.trim();
+                });
+                    
 
-              //去掉info中的空值
-              info = info.filter(function (s) {
-                return s && s.trim();
-              });
-
-
-
-              if (count != 1) {
-                if (info.length == 9 * count - 2) {
-                  for (i = 0; i < count * 4 - 1; i++) {
-                    info[i] = ""
-                  }
-                } else {
-                  for (i = 0; i < count; i++) {
-                    if (info.length == 7 * count + 2 * i - 2) {
-                      var j = 0;
-                      for (j = 0; j < 3 * count + i - 1; j++) {
-                        info[j] = ""
+                    if(count != 1){
+                      
+                      if(info.length == 9*count-2){
+                        for(i = 0;i<count*4-1;i++){
+                          info[i] = ""
+                        }
+                      }else{
+                        for(i = 0;i<count;i++){
+                          if(info.length == 7*count+2*i-2){
+                            var j = 0;
+                            for(j = 0;j<3*count+i-1;j++){
+                              info[j] = ""
+                            }
+                          }
+                        }
+                        
+                      }
+                      
+                    }else if(info.length == 5){
+                      for(i = 0;i<2;i++){
+                        info[i] = ""
+                      }
+                    }else{
+                      if(info[3].substr(0, 2) == "体育"){
+                        info[3] = info[3]+info[4]
+                        info[4] = "" 
+                      }
+                      for(i = 0;i<3;i++){
+                        info[i] = ""
                       }
                     }
-                  }
-                }
+                    
+                    //去掉info中的空值
+                    info = info.filter(function (s) {
+                       return s && s.trim();
+                    });
+                    
 
-              } else if (info.length == 5) {
-                for (i = 0; i < 2; i++) {
-                  info[i] = ""
-                }
-              } else {
-                if(info[3].substr(0, 2) == "体育"){
-                  info[3] = info[3]+info[4]
-                  info[4] = "" 
-                }
-                for(i = 0;i<3;i++){
-                  info[i] = ""
-                }
-              }
+                    
+                    //如果一个框框只有一门课
+                    if (count == 1){
+                      let course = getCourse(info,col);
+                      result.push(course)
+                    }else{
+                      let j = 0
+                      let z = 0
+                      let f = 0
+                      let temp = {}
+                      let son = []
+                      info[info.length] = '---------------------'
 
-              //去掉info中的空值
-              info = info.filter(function (s) {
-                return s && s.trim();
-              });
+                      for(j = 0;j<=info.length+1;j++){
+                        
+                        if(info[j] != '---------------------'){
+                           son[z] = info[j]
+                           z = z+1
+                        }else if(info[j] == '---------------------'){
 
+                          let course = getCourse(son,col)
 
+                          //一个框框里课程完全相同，但是周数不同
+                          // 获取result的长度
+                          let result_len = result.length
 
-              //如果一个框框只有一门课
-              if (count == 1) {
-                let course = getCourse(info, col);
+                          if(result[result_len-1]['name'] == course['name'] && result[result_len-1]['position'] == course['position'] && result[result_len-1]['teacher'] == course['teacher'] && result[result_len-1]['weeks'].toString() != course['weeks'] && result[result_len-1]['day'] == course['day'] ){
+                            //然后把course里的weeks和result[result_len-1]的weeks合并
+                            result[result_len-1]['weeks'] = result[result_len-1]['weeks'].concat(course['weeks'])
+                            console.log(result[result_len-1]['weeks'])
+                          }else{
+                            result.push(course)
+                          }
+                          z = 0
+                          son = []
+                        }
 
-                result.push(course)
-              } else {
-                let j = 0
-                let z = 0
-                let f = 0
-                let temp = {}
-                let son = []
-                info[info.length] = '---------------------'
-
-                for (j = 0; j <= info.length + 1; j++) {
-
-                  if (info[j] != '---------------------') {
-                    son[z] = info[j]
-                    z = z + 1
-                  } else if (info[j] == '---------------------') {
-
-                    let course = getCourse(son, col)
-                    result.push(course)
-                    z = 0
-                    son = []
-                  }
-
-                }
+                      }
+                    }
               }
             }
-          }
-
+          
         })
       }
     })
@@ -202,50 +213,50 @@ function scheduleHtmlParser(html) {
     "section": 1,
     "startTime": "08:00",
     "endTime": "08:45"
-  }, {
+  },{
     "section": 2,
     "startTime": "08:50",
     "endTime": "09:35"
-  }, {
+  },{
     "section": 3,
     "startTime": "09:55",
     "endTime": "10:40"
-  }, {
+  },{
     "section": 4,
     "startTime": "10:45",
     "endTime": "11:30"
-  }, {
+  },{
     "section": 5,
     "startTime": "13:10",
     "endTime": "13:55"
-  }, {
+  },{
     "section": 6,
     "startTime": "14:00",
     "endTime": "14:45"
-  }, {
+  },{
     "section": 7,
     "startTime": "15:00",
     "endTime": "15:45"
-  }, {
+  },{
     "section": 8,
     "startTime": "15:50",
     "endTime": "16:35"
-  }, {
+  },{
     "section": 9,
     "startTime": "16:50",
     "endTime": "17:35"
-  }, {
+  },{
     "section": 10,
     "startTime": "17:40",
     "endTime": "18:25"
-  }, {
+  },{
     "section": 11,
     "startTime": "19:10",
     "endTime": "19:55"
-  }, {
+  },{
     "section": 12,
     "startTime": "20:00",
     "endTime": "20:45"
   }]
-  return { courseInfos: result, sectionTimes: sectionTimes }
+  return { courseInfos: result,sectionTimes:sectionTimes }
 }
--- scheduleHtmlProvider.js
+++ scheduleHtmlProvider.js
@@ -1,9 +1,51 @@
-function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
-  const ifrs = dom.getElementsByTagName("iframe")
-  if (ifrs.length) {
-    for (let i = 1; i < ifrs.length; i++) {
-      iframeContent = ifrs[i].contentDocument.getElementById('Form1').innerHTML;
-    }
+//判断变量是否为空
+function isEmpty(param){
+  if(param){
+      var param_type = typeof(param);
+      if(param_type == 'object'){
+          //要判断的是【对象】或【数组】或【null】等
+          if(typeof(param.length) == 'undefined'){
+              if(JSON.stringify(param) == "{}"){
+                  return true;//空值，空对象
+              }
+          }else if(param.length == 0){
+              return true;//空值，空数组
+          }
+      }else if(param_type == 'string'){
+          //如果要过滤空格等字符
+          var new_param = param.trim();
+          if(new_param.length == 0){
+              //空值，例如:带有空格的字符串" "。
+              return true;
+          }
+      }else if(param_type == 'boolean'){
+          if(!param){
+              return true;
+          }
+      }else if(param_type== 'number'){
+          if(!param){
+              return true;
+          }
+      }
+      return false;//非空值
+  }else{
+      //空值,例如：
+      //(1)null
+      //(2)可能使用了js的内置的名称，例如：var name=[],这个打印类型是字符串类型。
+      //(3)空字符串''、""。
+      //(4)数字0、00等，如果可以只输入0，则需要另外判断。
+      return true;
   }
-  return iframeContent;
+}
+
+function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
+const ifrs = dom.getElementsByTagName("iframe")
+if(ifrs.length){
+    for(let i = 1;i<ifrs.length;i++){
+        if(!isEmpty(ifrs[i].contentDocument.getElementById('Form1'))){
+          iframeContent = ifrs[i].contentDocument.getElementById('Form1').innerHTML;
+        }
+    }
+}
+return iframeContent;
 }
